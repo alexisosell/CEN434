ee2d72d8bf6a52187c3fbbeb8421f9ec73592750
alexisosell <168184027+alexisosell@users.noreply.github.com>
2025-01-17 08:15:20 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Students (
+    ID INT,
+    Name VARCHAR(50),
+    MatriculationNumber VARCHAR(20),
+    Gender VARCHAR(7) 
+);
+INSERT INTO Students (ID, Name, MatriculationNumber, Gender) 
+VALUES (200580, 'Shai-Gilgeous Alexander', '20CJ000001', 'Male'),
+       (215008, 'Greg Popovich', '21CJ00002', 'Male'),
+       (215004, 'Justin Jefferson', '21CJ00003', 'Male'),
+       (210506, 'Patrick Mahomes', '21CJ00006', 'Male'),
+       (110005, 'Lamar Jackson', '11CJ00007', 'Male'),
+       (200062, 'Josh Allen', '20CJ05005', 'Male'),
+       (290008, 'Stephen A Smith', '13CJ05007', 'Male'),
+       (240013, 'Justin Herbert', '24CJ05008', 'Male'),
+       (240006, 'Saquon Barkley', '20CJ00509', 'Female'),
+       (150009, 'Amon-ra St.Brown', '15CJ00510', 'Female');
